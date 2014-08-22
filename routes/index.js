c70a2af9f661edf5941e3e49f30af6306d490fc7
Reed Wilson <reedcwilson@gmail.com>
2014-08-22 15:07:48 -0600

    initial commit

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET home page. */
+router.get('/j2k', function(req, res) {
+  res.render('j2k', { title: 'J2K Decoding' });
+});
+
 module.exports = router;
